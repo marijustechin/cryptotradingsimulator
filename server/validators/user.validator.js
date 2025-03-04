f049c3b186fb8555cf41d23225100e8a4c3e0712
marijustechin <marijus.smiginas@stud.technin.lt>
2025-03-04 11:02:57 +0400

    Limited special characters in email address

--- server/validators/user.validator.js
+++ server/validators/user.validator.js
@@ -7,7 +7,12 @@ exports.register = [
     .withMessage(
       'First name should be at least 3 characters lenght, and max 30 characters'
     ),
-  body('email').trim().isEmail().withMessage('Invalid email format'),
+  body('email')
+    .trim()
+    .isEmail()
+    .withMessage('Invalid email format')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    .withMessage('Invalid email characters'),
   body('password')
     .trim()
     .isStrongPassword({
