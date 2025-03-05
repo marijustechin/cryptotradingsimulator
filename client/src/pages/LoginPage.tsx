e5730be74b2f0d4f0d21fd8891d5d6a8cbe8156f
marijustechin <marijus.smiginas@stud.technin.lt>
2025-03-05 18:05:05 +0400

    Login form design test

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import { LoginForm } from '../components/auth/LoginForm';
 import { selectUser } from '../store/features/user/authSlice';
 import { useAppSelector } from '../store/store';
 import { useNavigate } from 'react-router';
+import bg from '/forms-background.png';
 
 export const LoginPage = () => {
   const user = useAppSelector(selectUser);
@@ -13,8 +14,13 @@ export const LoginPage = () => {
   }, [navigate, user]);
 
   return (
-    <main>
-      <div className="w-3xl mx-auto mt-10">
+    <main className="relative max-w-3xl mx-auto">
+      <img
+        className="absolute -left-60 md:top-30 z-10"
+        src={bg}
+        alt="background"
+      />
+      <div className="absolute sm:top-10 md:top-20 lg:top-32 md:right-10 z-20">
         <LoginForm />
       </div>
     </main>
