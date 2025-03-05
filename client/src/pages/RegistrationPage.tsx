5aef24ecee78c9d0d3a09f2b9aaba2c80d59a709
Laima <kulbyte.laima@gmail.com>
2025-03-05 13:18:21 +0200

    updates on CSS

--- client/src/pages/RegistrationPage.tsx
+++ client/src/pages/RegistrationPage.tsx
@@ -3,9 +3,7 @@ import { SignupForm } from "../components/auth/SignupForm";
 export const RegistrationPage = () => {
   return (
     <main>
-      <div className="w-3xl border border-violet-600 mx-auto mt-10">
-        <SignupForm/>
-      </div>
+      <SignupForm />
     </main>
   );
 };
