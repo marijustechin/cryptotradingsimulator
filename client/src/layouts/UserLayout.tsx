61393843b2f410189f854a760f4e12d2da4e7f5e
Sashabae <apigusova64@gmail.com>
2025-03-14 11:31:26 +0300

    Fixed sidebar overlapping footer

--- client/src/layouts/UserLayout.tsx
+++ client/src/layouts/UserLayout.tsx
@@ -1,22 +1,22 @@
-import { Outlet, useNavigate } from 'react-router';
-import { Footer } from '../components/Footer';
-import { Header } from '../components/header/Header';
-import { useAppSelector } from '../store/store';
-import { selectUser } from '../store/features/user/authSlice';
-import { useEffect } from 'react';
-import { Sidebar } from '../components/sidebar/Sidebar';
-import { userLinks } from '../components/sidebar/userLinks';
+import { Outlet, useNavigate } from "react-router";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/header/Header";
+import { useAppSelector } from "../store/store";
+import { selectUser } from "../store/features/user/authSlice";
+import { useEffect } from "react";
+import { Sidebar } from "../components/sidebar/Sidebar";
+import { userLinks } from "../components/sidebar/userLinks";
 
 export const UserLayout = () => {
   const navigate = useNavigate();
   const user = useAppSelector(selectUser);
 
   useEffect(() => {
-    if (user.role !== 'USER') navigate('/login');
+    if (user.role !== "USER") navigate("/login");
   }, [user, navigate]);
 
   return (
-    <div className="container">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex">
         <Sidebar navLinks={userLinks} />
@@ -24,7 +24,11 @@ export const UserLayout = () => {
           <Outlet />
         </main>
       </div>
-      <Footer />
+      <div className="pt-8">
+        <div className="w-full border-t border-[#636363]">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
