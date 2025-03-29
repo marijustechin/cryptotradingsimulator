ad6395546693c1a42df31d818cb3c15914e69b27
marijustechin <marijus.smiginas@stud.techin.lt>
2025-03-29 19:55:44 +0200

    Implemented initial assets functionality

--- client/src/types/order.ts
+++ client/src/types/order.ts
@@ -10,4 +10,23 @@ export interface IOpenOrder {
   open_date: string;
 }
 
+export interface IOrdersHistory {
+  triggerPrice: 1840;
+  id: number;
+  assetName: string;
+  ord_type: string;
+  ord_direct: string;
+  price: number;
+  amount: number;
+  orderValue: number;
+  open_date: string;
+}
+
+export interface IUserAssets {
+  asset: string;
+  balance: number;
+  spotCost: number;
+  avgBuyPrice: number;
+}
+
 //export interface IUserAssets {}
