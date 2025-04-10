96f99c4c523f4da55a57c13dda1e24cab78050de
Deivid Didiul <didiuldeivid@gmail.com>
2025-04-10 10:28:37 +0300

    User dashboard first concept

--- client/src/pages/user/UserDashboardPage.tsx
+++ client/src/pages/user/UserDashboardPage.tsx
@@ -1,8 +1,11 @@
+import UserBalanceChart from "../../components/dashboard/UserBalanceChart";
+import Inventory from "../../components/dashboard/Inventory";
+
 const UserDashboardPage = () => {
   return (
     <main className="mt-5">
-      <h1>User Dashboard</h1>
-      <p>Čia turi būti naudotojo informacijos suvestinė</p>
+      <UserBalanceChart />
+      <Inventory />
     </main>
   );
 };
